daa24bece89e18436f57922a785e8bdf2236fe74
Nikhila kalathuru <nikkinikhila95@gmail.com>
2022-10-02 19:59:41 -0400

    spec fixes

--- spec/controllers/weather_controller_spec.rb
+++ spec/controllers/weather_controller_spec.rb
@@ -4,39 +4,33 @@ RSpec.describe WeatherController, type: :controller do
   describe "GET weather data by Zipcode" do
     it "returns error message if invalid params are passed" do
       params = {}
-      get :show, params
+      get :show, params: params
 
       expect(@controller.instance_variable_get(:@error_message)).to eq("please enter a valid US 5 digit zipcode")
     end
 
     it "returns error message if invalid params are passed" do
-      params = { 'zipcode' => '2201d', 'measurement_unit' => 'Fahrenheit'}
-      get :show, params
+      params = { 'zipcode' => '2201d', 'unit' => 'Fahrenheit'}
+      get :show, params: params
 
       expect(@controller.instance_variable_get(:@error_message)).to eq("please enter a valid US 5 digit zipcode")
     end
 
     it "returns city not found if invalid zipcode is sent" do
-        params = { 'zipcode' => '00000', 'measurement_unit' => 'Fahrenheit'}
-        get :show, params: params
-
-        expect(@controller.instance_variable_get(:@current_weather_data)).to eq({"cod"=>"404", "message"=>"city not found"})
-      end
+      params = { 'zipcode' => '00000', 'unit' => 'Fahrenheit'}
+      get :show, params: params
+      expect(@controller.instance_variable_get(:@current_weather_data)).to eq({"cod"=>"404", "message"=>"city not found"})
     end
 
     it "returns valid response if valid zipcode is sent" do
-      VCR.use_cassette("weather/valid_zip", record: :once) do
-        params = { 'zipcode' => '95014', 'measurement_unit' => 'Fahrenheit'}
-        get :show, params: params
-
-        expect(@controller.instance_variable_get(:@current_weather_data)['name']).to eq('Cupertino')
-      end
+      params = { 'zipcode' => '95014', 'unit' => 'Fahrenheit'}
+      get :show, params: params
+      expect(@controller.instance_variable_get(:@current_weather_data)['name']).to eq('Cupertino')
     end
 
     it "returns imperial as unit when Fahrenheit measurement_unit is passed" do
-      params = { 'zipcode' => '95014', 'measurement_unit' => 'Fahrenheit'}
+      params = { 'zipcode' => '95014', 'unit' => 'Fahrenheit'}
       get :show, params: params
-
       expect(@controller.instance_variable_get(:@measurement_degree)).to eq('°F')
     end
   end
